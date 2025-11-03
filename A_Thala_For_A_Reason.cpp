452c9cee76e2353738bd05ee0fc134f1507773dc
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-11-03 20:33:08 +0600

    Solved first problem

---
+++ A_Thala_For_A_Reason.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int inputValue;
+    cin >> inputValue;
+
+    if (inputValue == 7)
+    {
+        cout << "THALA";
+    }
+    else
+    {
+        cout << "SADGE";
+    }
+
+    return 0;
+}
