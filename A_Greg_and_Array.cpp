387818b8994be5e554bf9d8a3c2b733d18a3123b
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-11-05 20:25:28 +0600

    Solving Greg and Array problem

---
+++ A_Greg_and_Array.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int vectorSize, operations, queries;
+    cin >> vectorSize >> operations >> queries;
+
+    vector<long long int> integerVector(vectorSize + 2), changeVector(vectorSize + 2, 0), queryVector(operations + 2, 0);
+
+    vector<pair<pair<long long int, long long int>, long long int>> pairVector(operations + 2);
+
+    for (int i = 1; i <= vectorSize; i++)
+    {
+        cin >> integerVector[i];
+    }
+
+    for (int i = 1; i <= operations; i++)
+    {
+        cin >> pairVector[i].first.first >> pairVector[i].first.second >> pairVector[i].second;
+    }
+
+    while (queries--)
+    {
+        int leftIndex, rightIndex;
+        cin >> leftIndex >> rightIndex;
+
+        queryVector[leftIndex]++;
+        queryVector[rightIndex + 1]--;
+    }
+
+    long long int count = 0;
+    for (int i = 1; i <= operations; i++)
+    {
+        count = count + queryVector[i];
+        changeVector[pairVector[i].first.first] = changeVector[pairVector[i].first.first] + (pairVector[i].second * count);
+        changeVector[pairVector[i].first.second + 1] = changeVector[pairVector[i].first.second] - (pairVector[i].second * count);
+    }
+
+    return 0;
+}
