5fd48262e9532287cadcb1ffd676c57b0590c25f
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-11-06 23:39:17 +0600

    Solving pair programming

---
+++ C_Pair_Programming.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int testcase;
+    cin >> testcase;
+
+    while (testcase--)
+    {
+        int k, n, m;
+        cin >> k >> n >> m;
+        
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        vector<int> b(m);
+        for (int i = 0; i < m; i++)
+        {
+            cin >> b[i];
+        }
+        
+        
+    }
+
+    return 0;
+}
